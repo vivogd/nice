2f7f61e9958adaec94616c66385d4184081e66f6
vivogd <aheilweil@gmail.com>
2023-12-06 12:34:54 +0200

    virtual keyboard

--- Build/nice.framework.js
+++ Build/nice.framework.js
@@ -1975,13 +1975,13 @@ var tempI64;
 // === Body ===
 
 var ASM_CONSTS = {
-  3897920: function() {Module['emscripten_get_now_backup'] = performance.now;},  
- 3897975: function($0) {performance.now = function() { return $0; };},  
- 3898023: function($0) {performance.now = function() { return $0; };},  
- 3898071: function() {performance.now = Module['emscripten_get_now_backup'];},  
- 3898126: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
- 3898187: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
- 3898251: function() {return Module.webglContextAttributes.powerPreference;}
+  3898208: function() {Module['emscripten_get_now_backup'] = performance.now;},  
+ 3898263: function($0) {performance.now = function() { return $0; };},  
+ 3898311: function($0) {performance.now = function() { return $0; };},  
+ 3898359: function() {performance.now = Module['emscripten_get_now_backup'];},  
+ 3898414: function() {return Module.webglContextAttributes.premultipliedAlpha;},  
+ 3898475: function() {return Module.webglContextAttributes.preserveDrawingBuffer;},  
+ 3898539: function() {return Module.webglContextAttributes.powerPreference;}
 };
 
 
